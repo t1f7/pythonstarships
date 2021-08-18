170a1e7097fe7a25024a4a8079b680cba2543aaf
t1f7 <11868838+t1f7@users.noreply.github.com>
2021-08-18 10:42:56 +0300

    figured it out

--- sdk/dotnet.py
+++ sdk/dotnet.py
@@ -4,10 +4,7 @@ class DotNet(object):
 
     @classmethod
     def validDateTime(self):
-        # I can't remember why I did this, but this could be a valid reason
-        # why code won't work on your PC
-        # timezone must be UTC (maybe)
-        return datetime.now() - timedelta(hours=3)
+        return datetime.utcnow()
 
     @classmethod
     def ticks(self, dt):
